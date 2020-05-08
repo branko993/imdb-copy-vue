799e4b42652e02a462199be49a2e409509bc6cd4
branko993 <branko993@gmail.com>
2020-05-08 17:24:42 +0200

    small style change

--- src/components/WatchListReactions.vue
+++ src/components/WatchListReactions.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="form-inline" v-if="watchList">
     <div v-if="watchList[0]" style="right:30px; position:absolute">
-      <h6 v-show="watchList[0].watched === 1">You watched this movie!</h6>
-      <h6 v-show="watchList[0].watched === 0">You did't watch this movie!</h6>
+      <h6 v-show="watchList[0].watched === 1" style="color:green">You watched this movie!</h6>
+      <h6 v-show="watchList[0].watched === 0" style="color:red">You did't watch this movie!</h6>
     </div>
     <div v-if="!watchList[0]" style="right:0; position:absolute">
       <input
